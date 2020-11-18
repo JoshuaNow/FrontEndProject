bd238bac7c890710a20a259f9dfaddc1e994cfef
Eunice Pereira <eunicepere333@gmail.com>
2020-11-18 15:53:37 -0600

    css and js update

--- cardFunction.js
+++ cardFunction.js
@@ -1,48 +1,45 @@
 // JavaScript Document
 
-export const createCard =(event)=>{
-    //DOM Elements
-        let divThree = document.getElementById("gridItem3")
+export const createCard = (event) => {
+	//DOM Elements
+	let divThree = document.getElementById('gridItem3');
 
-        //Create Article Card
-        let cardDivElement = document.createElement("div");
-        cardDivElement.classList.add('newsCard');
-    
-        //Article Link
-        let a = document.createElement("a");
-        a.href = event.url;
-        
-        //Card Content
-        let sectionElement = document.createElement("section");
-        let sectionTitle = document.createElement("h4")
-        let sectionDescription = document.createElement("p");
-        let pElement = document.createElement("p");
-        let imgElement = document.createElement("img");
-        
-        let images = event.multimedia;
-        images.forEach((img) =>{
-            imgElement.src = img.url;
-            return imgElement;
-        });
-    
-        //Appending Card Elements
-        sectionTitle.innerHTML = event.title;
-        sectionDescription.innerHTML = event.abstract;
-        cardDivElement.append(imgElement, sectionTitle, pElement, sectionDescription);
-        divThree.append(cardDivElement);
-    
-        //EventListener
-        
-    
-        cardDivElement.addEventListener("click",(event)=>{
-            window.location = a.href;
-        })
-        //divThree.append(cardDivElement);
-        console.log(cardDivElement)
-        console.log(event);
-        
-      
-     };
+	//Create Article Card
+	let cardDivElement = document.createElement('div');
+	cardDivElement.classList.add('newsCard');
+
+	//Article Link
+	let a = document.createElement('a');
+	a.href = event.url;
+
+	//Card Content
+	let sectionElement = document.createElement('section');
+	let sectionTitle = document.createElement('h4');
+	let sectionDescription = document.createElement('p');
+	let pElement = document.createElement('p');
+	let imgElement = document.createElement('img');
+
+	let images = event.multimedia;
+	images.forEach((img) => {
+		imgElement.src = img.url;
+		return imgElement;
+	});
+
+	//Appending Card Elements
+	sectionTitle.innerHTML = event.title;
+	sectionDescription.innerHTML = event.abstract;
+	cardDivElement.append(imgElement, sectionTitle, pElement, sectionDescription);
+	divThree.append(cardDivElement);
+
+	//EventListener
+
+	cardDivElement.addEventListener('click', (event) => {
+		window.location = a.href;
+	});
+	//divThree.append(cardDivElement);
+	console.log(cardDivElement);
+	console.log(event);
+};
 
 //export const cocktailCards =(event)=>{
 //    //DOM Elements
@@ -52,24 +49,24 @@ export const createCard =(event)=>{
 //        //Create Article Card
 //        let cardDivElement = document.createElement("div");
 //        cardDivElement.id = 'cardDiv';
-//    
-//        
+//
+//
 //        //Article Link
 //        let a = document.createElement("a");
 //        a.href = event.webUrl;
-//        
+//
 //        //Card Content
 //        let sectionElement = document.createElement("section");
 //        let sectionTitle = document.createElement("h4")
 //        let sectionDescription = document.createElement("p");
 //        let pElement = document.createElement("p");
 //        let imgElement = document.createElement("img");
-//        
+//
 //        sectionTitle.innerHTML = event.webTitle;
 //        mainDiv.append(cardDivElement);
 //        cardDivElement.append(sectionElement);
 //        sectionElement.append(imgElement, sectionTitle, pElement, sectionDescription);
-//    
-//        
-//    
+//
+//
+//
 //}
--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 import ajax from './ajax.js';
-import {createCard} from './cardFunction.js';
-
+import { createCard } from './cardFunction.js';
 
 //let main = document.querySelector("main");
 //let mainDiv = document.createElement("div")
@@ -36,125 +35,124 @@ let body = document.querySelector('body');
 //		//console.log(resArr)
 //	}
 //);
-const getCocktails =()=>{
-ajax('https://www.thecocktaildb.com/api/json/v1/1/random.php', (res) => {
-	let resArr2 = JSON.parse(res).drinks;
-	resArr2.forEach((drink) => {
-		// Cocktail API DOM elements
-		let drinkTitle = drink.strDrink;
-		let drinkCategory = drink.strCategory;
-		let drinkGlass = drink.strGlass;
-		let drinkInstruct = drink.strInstructions;
+const getCocktails = () => {
+	ajax('https://www.thecocktaildb.com/api/json/v1/1/random.php', (res) => {
+		let resArr2 = JSON.parse(res).drinks;
+		resArr2.forEach((drink) => {
+			// Cocktail API DOM elements
+			let drinkTitle = drink.strDrink;
+			let drinkCategory = drink.strCategory;
+			let drinkGlass = drink.strGlass;
+			let drinkInstruct = drink.strInstructions;
 
-		// create image element + url
-		let drinkImage = document.createElement('img');
-		drinkImage.setAttribute('id', 'drink-image');
-		drinkImage.src = drink.strDrinkThumb;
+			// create image element + url
+			let drinkImage = document.createElement('img');
+			drinkImage.setAttribute('id', 'drink-image');
+			drinkImage.src = drink.strDrinkThumb;
 
-		// add button to generate cocktails
-		const buttonDrink = document.createElement('button');
-		buttonDrink.setAttribute('id', 'drink-button');
-		buttonDrink.addEventListener('click', (evt) => {
-            
-        getCocktails();
-            
-		});
+			// add button to generate cocktails
+			const buttonDrink = document.createElement('button');
+			buttonDrink.setAttribute('id', 'drink-button');
+			buttonDrink.addEventListener('click', (evt) => {
+				getCocktails();
+			});
 
-		// generate cocktail info
-		let drinkInfoList = document.createElement('li');
-		drinkInfoList.setAttribute('id', 'drink-info');
-		drinkInfoList.innerHTML = `
-		Cocktail: ${drinkTitle}
-		<li>Category: ${drinkCategory}</li>
-		<li>Glass: ${drinkGlass}</li>
-		<li>Ingredients:</li>
+			// generate cocktail info
+			let drinkInfoList = document.createElement('li');
+			drinkInfoList.setAttribute('id', 'drink-info');
+			drinkInfoList.innerHTML = `
+		<b>Cocktail:</b> ${drinkTitle}
+		<li><b>Category:</b> ${drinkCategory}</li>
+		<li><b>Glass:</b> ${drinkGlass}</li>
+		<li><b>Ingredients:</b></li>
 		<li>${drink.strIngredient1}, ${drink.strMeasure1}</li>
 		<li>${drink.strIngredient2}, ${drink.strMeasure2}</li>
 		<li>${drink.strIngredient3}, ${drink.strMeasure3}</li>
 		<li>${drink.strIngredient4}, ${drink.strMeasure4}</li>
 		<li>${drink.strIngredient5}, ${drink.strMeasure5}</li>
 		<li>${drink.strIngredient6}, ${drink.strMeasure6}</li>
-		<li>Make it: ${drinkInstruct}</li>
+		<li><b>Make it:</b></li>
+		<li>${drinkInstruct}</li>
 		`;
-		let filteredList = function (obj) {
-			for (var empty in drinkInfoList) {
-				if (drink.strMeasure6 === null) {
-					delete drink.strMeasure6;
-					return filteredList;
+			let filteredList = function (obj) {
+				for (var empty in drinkInfoList) {
+					if (drink.strMeasure6 === null) {
+						delete drink.strMeasure6;
+						return filteredList;
+					}
 				}
-			}
-		};
-		// append button to drink list
-		buttonDrink.innerHTML = 'New Cocktail Recipe!';
-		drinkInfoList.append(buttonDrink);
+			};
+			// append button to drink list
+			buttonDrink.innerHTML = 'New Cocktail Recipe';
+			drinkInfoList.append(buttonDrink);
 
-		// create cocktail card + append to page
-		let drinkCard = document.createElement('div');
-		drinkCard.setAttribute('class', 'drink-card');
-		drinkCard.append(drinkInfoList, drinkImage);
-        
-        let divTwo = document.getElementById("gridItem2")
-        divTwo.innerHTML = "";
-		divTwo.append(drinkCard);
+			// create cocktail card + append to page
+			let drinkCard = document.createElement('div');
+			drinkCard.setAttribute('class', 'drink-card');
+			drinkCard.append(drinkInfoList, drinkImage);
 
-		console.log(filteredList);
+			let divTwo = document.getElementById('gridItem2');
+			divTwo.innerHTML = '';
+			divTwo.append(drinkCard);
+
+			console.log(filteredList);
+		});
 	});
-});
-}
+};
 getCocktails();
 
 //getNewArticles
-ajax("https://api.nytimes.com/svc/topstories/v2/food.json?api-key=Uou4anPgDJHztfUSXGm7Ru1SiHzqpcv5"
-, res => {
-    let resArr = JSON.parse(res).results
-    resArr.filter((event)=>{
-        //console.log(event)
-        return event.section == "dining"//&& event.subsection == "food";
-    }).forEach((event, idx)=>{
-        if(idx >= 16) return;
-        createCard(event);
-        
-        
-//        //DOM Elements
-//        
-//        //Create Article Card
-//        let cardDivElement = document.createElement("div");
-//        cardDivElement.id = 'cardDiv';
-//        
-//        //Article Link
-//        let a = document.createElement("a");
-//        a.href = event.url;
-//        
-//        //Card Content
-//        let sectionElement = document.createElement("section");
-//        let sectionTitle = document.createElement("h4")
-//        let sectionDescription = document.createElement("p");
-//        let pElement = document.createElement("p");
-//        let imgElement = document.createElement("img");
-//        
-//        let images = event.multimedia;
-//        images.forEach((img) =>{
-//            imgElement.src = img.url;
-//            return imgElement;
-//        });
-//    
-//        //Appending Card Elements
-//        sectionTitle.innerHTML = event.title;
-//        sectionDescription.innerHTML = event.abstract;
-//        mainDiv.append(cardDivElement);
-//        cardDivElement.append(sectionElement);
-//        sectionElement.append(imgElement, sectionTitle, pElement, sectionDescription);
-//        
-//        
-//        console.log(event);
-        
-      
-     })
-    //as much JS code to do what I want
-     //document.body.append(resArr)
-    //console.log(resArr)
-     
-});
+ajax(
+	'https://api.nytimes.com/svc/topstories/v2/food.json?api-key=Uou4anPgDJHztfUSXGm7Ru1SiHzqpcv5',
+	(res) => {
+		let resArr = JSON.parse(res).results;
+		resArr
+			.filter((event) => {
+				//console.log(event)
+				return event.section == 'dining'; //&& event.subsection == "food";
+			})
+			.forEach((event, idx) => {
+				if (idx >= 16) return;
+				createCard(event);
+
+				//        //DOM Elements
+				//
+				//        //Create Article Card
+				//        let cardDivElement = document.createElement("div");
+				//        cardDivElement.id = 'cardDiv';
+				//
+				//        //Article Link
+				//        let a = document.createElement("a");
+				//        a.href = event.url;
+				//
+				//        //Card Content
+				//        let sectionElement = document.createElement("section");
+				//        let sectionTitle = document.createElement("h4")
+				//        let sectionDescription = document.createElement("p");
+				//        let pElement = document.createElement("p");
+				//        let imgElement = document.createElement("img");
+				//
+				//        let images = event.multimedia;
+				//        images.forEach((img) =>{
+				//            imgElement.src = img.url;
+				//            return imgElement;
+				//        });
+				//
+				//        //Appending Card Elements
+				//        sectionTitle.innerHTML = event.title;
+				//        sectionDescription.innerHTML = event.abstract;
+				//        mainDiv.append(cardDivElement);
+				//        cardDivElement.append(sectionElement);
+				//        sectionElement.append(imgElement, sectionTitle, pElement, sectionDescription);
+				//
+				//
+				//        console.log(event);
+			});
+		//as much JS code to do what I want
+		//document.body.append(resArr)
+		//console.log(resArr)
+	}
+);
 
 //ajax("https://content.guardianapis.com/search?show-elements=image&q=cocktails&api-key=6776e4cc-c284-4535-9eee-8901e1809648"
 //	, res => {
@@ -162,8 +160,8 @@ ajax("https://api.nytimes.com/svc/topstories/v2/food.json?api-key=Uou4anPgDJHztf
 //		console.log(resArr2)
 //		resArr2.forEach((event) => {
 //			cocktailCards(event)
-//        
-//        
+//
+//
 //	})
-//    
+//
 //});
